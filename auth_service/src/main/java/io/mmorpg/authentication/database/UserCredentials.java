fdb234a83336749875e7b9401c0f28e43c343164
Marco Bocchetti <marcobocchetti3@gmail.com>
2026-02-16 20:14:27 +0100

    [Auth Service] Add UserCredentials entity and repository
    Used to access usernames and passwords stored in PostgreSQL

---
+++ auth_service/src/main/java/io/mmorpg/authentication/database/UserCredentials.java
@@ -0,0 +1,33 @@
+package io.mmorpg.authentication.database;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "user_credentials")
+public class UserCredentials {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+}
