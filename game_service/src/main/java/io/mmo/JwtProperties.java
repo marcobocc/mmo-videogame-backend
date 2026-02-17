798c0d598eda53bae5a89b0ad786a330f959c69f
Marco Bocchetti <marcobocchetti3@gmail.com>
2026-02-17 11:40:22 +0100

    [Game Service] Remove JWT expiration property from game service validator

--- game_service/src/main/java/io/mmo/JwtProperties.java
+++ game_service/src/main/java/io/mmo/JwtProperties.java
@@ -9,5 +9,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
     private String secret;
-    private long expiration;
 }
